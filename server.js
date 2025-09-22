4b1fcb9c92780fd66fb0076027b97a54dd7f1776
Ebadur Rahman <ebadur.rahman@sjinnovation.com>
2025-09-22 11:04:32 +0600

    Implement database connection and update server initialization

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require("express");
 require("dotenv").config();
+const connectDB = require("./db/connectDB");
 
 const app = express();
 
 // middleware
 app.use(express.json());
 
+// routes
 app.get("/", (req, res) => {
-    res.send("Hello from the server");
+    res.send("Hello from the serverr");
 });
 
+// port
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        console.log("Connected to DB");
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();

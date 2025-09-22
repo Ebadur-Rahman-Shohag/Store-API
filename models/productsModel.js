77dfb2a3aefde5202f96a9496b84a28778d3ddba
Ebadur Rahman <ebadur.rahman@sjinnovation.com>
2025-09-22 19:28:54 +0600

    feat: add products model and initial product data

---
+++ models/productsModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productsSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: [true, "Please provide a price"],
+    },
+    company: {
+        type: String,
+        required: [true, "Please provide a company"],
+        enum: {
+            values: ["ikea", "liddy", "caressa", "marcos"],
+            message: "{VALUE} is not supported",
+        },
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    }
+
+});
